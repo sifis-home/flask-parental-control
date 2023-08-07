b5203a4847e8b14d6c90399ff1b51e6b4896061b
WisamAbbasi <39978331+WisamAbbasi@users.noreply.github.com>
2023-08-07 12:14:12 +0200

    edit test

--- test_flask-parental-control.py
+++ test_flask-parental-control.py
@@ -1,8 +1,56 @@
 import platform
 import unittest
+from pathlib import Path
 from unittest.mock import patch
 
+from omegaconf import OmegaConf
+from tensorflow.keras import applications
+from tensorflow.keras.layers import Dense
+from tensorflow.keras.models import Model
+from tensorflow.keras.utils import get_file
+
 from app import get_data, on_close, on_error, on_open, video_capture
+from factory import get_model
+
+pretrained_model = "https://github.com/yu4u/age-gender-estimation/releases/download/v0.6/EfficientNetB3_224_weights.11-3.44.hdf5"
+modhash = "6d7f7b7ced093a8b3ef6399163da6ece"
+
+
+def test_get_model():
+    weight_file = get_file(
+        "EfficientNetB3_224_weights.11-3.44.hdf5",
+        pretrained_model,
+        cache_subdir="pretrained_models",
+        file_hash=modhash,
+        cache_dir=str(Path(__file__).resolve().parent),
+    )
+
+    # load model and weights
+    model_name, img_size = Path(weight_file).stem.split("_")[:2]
+    img_size = int(img_size)
+    cfg = OmegaConf.from_dotlist(
+        [f"model.model_name={model_name}", f"model.img_size={img_size}"]
+    )
+
+    model = get_model(cfg)
+
+    base_model = getattr(applications, cfg.model.model_name)(
+        include_top=False,
+        input_shape=(cfg.model.img_size, cfg.model.img_size, 3),
+        pooling="avg",
+    )
+    features = base_model.output
+    pred_gender = Dense(units=2, activation="softmax", name="pred_gender")(
+        features
+    )
+    pred_age = Dense(units=101, activation="softmax", name="pred_age")(
+        features
+    )
+    expected_model = Model(
+        inputs=base_model.input, outputs=[pred_gender, pred_age]
+    )
+
+    assert model == expected_model
 
 
 def test_get_data():
